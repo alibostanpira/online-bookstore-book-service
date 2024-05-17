32835e00e8d5b749f40bda8a3408222ce112add8
Ali Bostanpira <alibostanpira@gmail.com>
2024-05-17 14:48:38 +0330

    Initialization

---
+++ src/main/java/online/bs/bookservice/OnlineBookstoreBookServiceApplication.java
@@ -0,0 +1,13 @@
+package online.bs.bookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookstoreBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookstoreBookServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/bs/bookservice/OnlineBookstoreBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package online.bs.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstoreBookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
